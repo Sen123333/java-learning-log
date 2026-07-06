536562f699938232aae1415ef7b7cf08c3f9544a
Sen123333 <sskcqw11@gmail.com>
2026-07-06 09:58:13 -0400

    Update GradeLevel.java

--- week01-java-basics/day02-variables-if-loop-method/src/GradeLevel.java
+++ week01-java-basics/day02-variables-if-loop-method/src/GradeLevel.java
@@ -1,1 +1,34 @@
 
+class Score{
+    int studentscore;
+}
+class Gradedetector{
+    void grade(){
+        Score one = new Score();
+        one.studentscore = 80;
+        if (one.studentscore >= 90){
+            System.out.println("A");
+        }
+        else if (one.studentscore >= 80){
+            System.out.println("B");
+        }
+         else if (one.studentscore >= 70){
+            System.out.println("C");
+        }
+         else if (one.studentscore >= 60){
+            System.out.println("D");
+        }
+         else{
+            System.out.println("F");
+        }
+    
+    }
+
+}
+public class GradeLevel {
+    public static void main(String[] args) {
+        Gradedetector two = new Gradedetector();
+        two.grade();
+    }
+    
+}
