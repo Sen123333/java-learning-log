76a687bfb81eece69d3fd86daa0d9728ee5365e2
Sen123333 <sskcqw11@gmail.com>
2026-07-06 09:58:30 -0400

    Update MaxOfThree.java

--- week01-java-basics/day02-variables-if-loop-method/src/MaxOfThree.java
+++ week01-java-basics/day02-variables-if-loop-method/src/MaxOfThree.java
@@ -1,1 +1,16 @@
+public class MaxOfThree {
+    public int findMax(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        return max;
+    }
 
+    public static void main(String[] args) {
+        MaxOfThree maxFinder = new MaxOfThree();
+        
+        System.out.println("Max of 5, 8, 3: " + maxFinder.findMax(5, 8, 3));
+        System.out.println("Max of 10, 10, 10: " + maxFinder.findMax(10, 10, 10));
+        System.out.println("Max of -1, -5, 0: " + maxFinder.findMax(-1, -5, 0));
+    }
+}
