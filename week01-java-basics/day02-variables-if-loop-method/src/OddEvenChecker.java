f83994179e7c47a4644b14e74e55a46d6f700eb2
Sen123333 <sskcqw11@gmail.com>
2026-07-06 09:59:16 -0400

    Update OddEvenChecker.java

--- week01-java-basics/day02-variables-if-loop-method/src/OddEvenChecker.java
+++ week01-java-basics/day02-variables-if-loop-method/src/OddEvenChecker.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+class Number{
+    void userinput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int usernumber = scanner.nextInt();
+        if (usernumber % 2 == 0){
+            System.out.println("The number you enter is an even number.");
+        }
+        else{
+             System.out.println("The number you enter is an odd number.");
+        }
+
+    }
+}
+    
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        Number one = new Number();
+        one.userinput();
+    }
+    
+}
 
