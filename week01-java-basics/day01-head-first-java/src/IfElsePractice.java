8bdffa4e22d0a080ae4a424845adbd5157e3b3c5
Sen123333 <sskcqw11@gmail.com>
2026-07-06 01:45:10 -0400

    Update IfElsePractice.java

--- week01-java-basics/day01-head-first-java/src/IfElsePractice.java
+++ week01-java-basics/day01-head-first-java/src/IfElsePractice.java
@@ -1,1 +1,28 @@
-
+public class IfElsePractice {
+    public static void main(String[] args) {
+        int score = 80;
+        int n = 4;
+        if (score >= 90){
+            System.out.println("A");
+        }
+        else if (score >= 80){
+            System.out.println("B");
+        }
+         else if (score >= 70){
+            System.out.println("C");
+        }
+         else if (score >= 60){
+            System.out.println("D");
+        }
+         else{
+            System.out.println("F");
+        }
+    
+         if (n % 2 == 0){
+            System.out.println("This number is not odd.");
+        }
+         else{
+            System.out.println("This number is odd.");
+         }
+    }
+}
