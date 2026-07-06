17d06e1000e510381c1ac9973047b1e698ce24ee
Sen123333 <sskcqw11@gmail.com>
2026-07-06 01:44:25 -0400

    Update LoopyModified.java

--- week01-java-basics/day01-head-first-java/src/LoopyModified.java
+++ week01-java-basics/day01-head-first-java/src/LoopyModified.java
@@ -1,1 +1,12 @@
-
+public class LoopyModified {
+    public static void main(String[] args) {
+        int x = 1;
+        int sum = 0;
+        while (x <= 10){
+            System.out.println(x);
+            sum = sum + x;
+            x = x + 1;
+    }
+        System.out.println("Sum is:"+ sum);
+}
+}
