d4763b44594faa79975035c810bc31426548372c
Sen123333 <sskcqw11@gmail.com>
2026-07-06 01:45:36 -0400

    Create PythonVsJavaNotes.java

---
+++ week01-java-basics/day01-head-first-java/src/PythonVsJavaNotes.java
@@ -0,0 +1,10 @@
+class PythonVsJavaNotes{
+    public static void main(String[] args) {
+        System.out.println("5 differences between Python and Java:");
+        System.out.println("Python syntax is simpler and easier to read. Java syntax is stricter and needs more code.");
+        System.out.println("Python is usually interpreted line by line. Java needs to be compiled first, then run.");
+        System.out.println("Python does not need you to write the variable type,Java needs the type");
+        System.out.println("Python code is usually shorter. Java code is usually longer because it needs classes and the main method.");
+        System.out.println("Python is often used for AI, data analysis, automation, and scripts. Java is often used for Android apps, \nlarge systems, and backend development.");
+    }
+}
