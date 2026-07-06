5c5e81ff6dd20c9d565f5678f97ace9f0fa54907
Sen123333 <sskcqw11@gmail.com>
2026-07-06 09:58:54 -0400

    Update MethodPractice.java

--- week01-java-basics/day02-variables-if-loop-method/src/MethodPractice.java
+++ week01-java-basics/day02-variables-if-loop-method/src/MethodPractice.java
@@ -1,1 +1,36 @@
+public class MethodPractice {
 
+    public boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public int maxOfThree(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        return max;
+    }
+
+    public long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MethodPractice practice = new MethodPractice();
+        
+        System.out.println("Is 4 even? " + practice.isEven(4));
+        System.out.println("Is 7 even? " + practice.isEven(7));
+        
+        System.out.println("Max of 5, 8, 3: " + practice.maxOfThree(5, 8, 3));
+        
+        System.out.println("Factorial of 5: " + practice.factorial(5));
+        System.out.println("Factorial of 0: " + practice.factorial(0));
+    }
+}
