bfbaf4b7d9cd45c23951f3e7e00797846eaa8f78
Sen123333 <sskcqw11@gmail.com>
2026-07-07 07:58:43 -0400

    Update Book.java

--- week01-java-basics/ day03-objectville/src /Book.java
+++ week01-java-basics/ day03-objectville/src /Book.java
@@ -1,1 +1,13 @@
-
+class Book{
+    String title;
+    String author;
+    int pages;
+    void isLongBook(){
+        if(pages<200){
+            System.out.println(title+" by "+author+" has "+pages+" pages."+"It's not a long book.");
+        }
+        else{
+             System.out.println(title+" by "+author+" has "+pages+" pages."+"It's a long book.");
+        }
+    }
+}
