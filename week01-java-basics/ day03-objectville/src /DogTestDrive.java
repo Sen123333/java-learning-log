838a9eb9fa93c5a433606b7065b75c1f6d66e837
Sen123333 <sskcqw11@gmail.com>
2026-07-07 07:59:48 -0400

    Update DogTestDrive.java

--- week01-java-basics/ day03-objectville/src /DogTestDrive.java
+++ week01-java-basics/ day03-objectville/src /DogTestDrive.java
@@ -1,1 +1,19 @@
-
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog one = new Dog();
+        one.name = "Max";
+        one.size = "100cm.";
+        one.breed = "German Shepherd";
+        one.bark();
+        Dog two = new Dog();
+        two.name = "Luna";
+        two.size = "103cm";
+        two.breed = "Golden Retriever";
+        two.bark();
+        Dog three = new Dog();
+        three.name = "Milo";
+        three.size = "90cm";
+        three.breed = "Bulldog";
+        three.bark();
+    }
+}
