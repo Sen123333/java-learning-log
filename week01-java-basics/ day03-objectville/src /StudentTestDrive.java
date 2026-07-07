4142172bf32daf5d8016ce6cab00414b0cf1c6db
Sen123333 <sskcqw11@gmail.com>
2026-07-07 08:00:40 -0400

    Update StudentTestDrive.java

--- week01-java-basics/ day03-objectville/src /StudentTestDrive.java
+++ week01-java-basics/ day03-objectville/src /StudentTestDrive.java
@@ -1,1 +1,24 @@
+public class StudentTestDrive {
+    public static void main(String[] args) {
+        
+    Student one = new Student();
+    one.name = "Tom";
+    one.age = 15;
+    one.score = 80;
+    one.printInfo();
+    one.isPassing();
+    Student two = new Student();
+    two.name = "Ben";
+    two.age = 16;
+    two.score = 50;
+    two.printInfo();
+    two.isPassing();
+    Student three = new Student();
+    three.name = "Ben";
+    three.age = 16;
+    three.score = 65;
+    three.printInfo();
+    three.isPassing();
+    }
+}
 
