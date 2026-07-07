365b0a23e5760f9ecec42a17f289f7dc49abb9c3
Sen123333 <sskcqw11@gmail.com>
2026-07-07 08:00:10 -0400

    Update SimpleGuessingGame.java

--- week01-java-basics/ day03-objectville/src /SimpleGuessingGame.java
+++ week01-java-basics/ day03-objectville/src /SimpleGuessingGame.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+class Number {
+    int answer;
+    int userInput;
+
+    void inputGuess() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        userInput = in.nextInt();
+    }
+    
+    void checkGuess() {
+        if (userInput < answer) {
+            System.out.println("Too low");
+        } else if (userInput == answer) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Too high");
+        }
+    }
+}
+
+public class SimpleGuessingGame {
+    public static void main(String[] args) {
+        Number game = new Number();
+        game.answer = 7;
+        game.inputGuess();
+        game.checkGuess();
+    }
+}
