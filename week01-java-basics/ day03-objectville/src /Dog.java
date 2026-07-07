d499bfd4c3e53c33ffdfaaf409ac799c070b152b
Sen123333 <sskcqw11@gmail.com>
2026-07-07 07:59:28 -0400

    Update Dog.java

--- week01-java-basics/ day03-objectville/src /Dog.java
+++ week01-java-basics/ day03-objectville/src /Dog.java
@@ -1,1 +1,8 @@
-
+class Dog{
+    String name;
+    String size;
+    String breed;
+    void bark(){
+        System.out.println(name +" "+"("+breed+")"+" is barking." + " Size: "+ size);
+    }
+}
