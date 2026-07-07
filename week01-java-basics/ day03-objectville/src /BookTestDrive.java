34e625608b1ba3b353db348b3da7714cf34323b6
Sen123333 <sskcqw11@gmail.com>
2026-07-07 07:59:02 -0400

    Update BookTestDrive.java

--- week01-java-basics/ day03-objectville/src /BookTestDrive.java
+++ week01-java-basics/ day03-objectville/src /BookTestDrive.java
@@ -1,1 +1,20 @@
-
+public class BookTestDrive {
+    public static void main(String[] args) {
+    
+    Book one = new Book();
+    one.title = "<All Tomorrows>";
+    one.author = "C. M. Kosemen";
+    one.pages = 150;
+    one.isLongBook();
+    Book two = new Book();
+    two.title = "<I Have No Mouth, and I Must Scream>";
+    two.author = "Harlan Ellison";
+    two.pages = 100;
+    two.isLongBook();
+    Book three = new Book();
+    three.title = "<1984>";
+    three.author = "George Orwell";
+    three.pages = 400;
+    three.isLongBook();
+    }
+}
