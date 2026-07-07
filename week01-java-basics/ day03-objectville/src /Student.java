8bfbf41992dde9f0d281da6f73ed2e5b1d8470fc
Sen123333 <sskcqw11@gmail.com>
2026-07-07 08:00:25 -0400

    Update Student.java

--- week01-java-basics/ day03-objectville/src /Student.java
+++ week01-java-basics/ day03-objectville/src /Student.java
@@ -1,1 +1,16 @@
-
+class Student{
+    String name;
+    int age;
+    int score;
+    void printInfo(){
+        System.out.println("Student "+name+" is "+age+" years old. His score is "+score);
+    }
+    void isPassing(){
+        if(score < 60){
+            System.out.println("Student did not pass the test.");
+        }
+        else{
+            System.out.println("Student passed the test.");
+        }
+        }
+}
