dfb93e8cd7d64a2a898a7612d90dfb8f6b84a707
Sen123333 <sskcqw11@gmail.com>
2026-07-09 06:47:41 -0400

    Update Book.java

--- week01-java-basics/   day04-variables-references/src/Book.java
+++ week01-java-basics/   day04-variables-references/src/Book.java
@@ -1,1 +1,17 @@
+public class Book {
+    String title;
+    String author;
+    int pages;
 
+    public boolean isLongBook() {
+        return pages >= 300;
+    }
+
+    public void printInfo() {
+        if (isLongBook()) {
+            System.out.println(title + " has " + pages + " pages. It is a long book.");
+        } else {
+            System.out.println(title + " has " + pages + " pages. It is not a long book.");
+        }
+    }
+}
