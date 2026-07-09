dcfff1e1026e30b69f5ad3f795a861deb2b4d703
Sen123333 <sskcqw11@gmail.com>
2026-07-09 06:48:06 -0400

    Update ReferenceDemo.java

--- week01-java-basics/   day04-variables-references/src/ReferenceDemo.java
+++ week01-java-basics/   day04-variables-references/src/ReferenceDemo.java
@@ -1,1 +1,21 @@
+public class ReferenceDemo {
+    public static void main(String[] args) {
+        Dog a = new Dog();
+        a.name = "Max";
+        a.size = 30;
+        a.breed = "Husky";
 
+        Dog b = a;
+
+        System.out.println("Before changing b.name:");
+        System.out.println("a.name = " + a.name);
+        System.out.println("b.name = " + b.name);
+
+        b.name = "Luna";
+
+        System.out.println();
+        System.out.println("After changing b.name:");
+        System.out.println("a.name = " + a.name);
+        System.out.println("b.name = " + b.name);
+    }
+}
