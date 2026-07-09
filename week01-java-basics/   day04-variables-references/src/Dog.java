342e81f80d9307f6edf91a36aa8f22afa27f476e
Sen123333 <sskcqw11@gmail.com>
2026-07-09 06:47:27 -0400

    Update Dog.java

--- week01-java-basics/   day04-variables-references/src/Dog.java
+++ week01-java-basics/   day04-variables-references/src/Dog.java
@@ -1,1 +1,15 @@
+public class Dog {
+    String name;
+    int size;
+    String breed;
 
+    public void bark() {
+        if (size < 20) {
+            System.out.println(name + " says: Woof!");
+        } else if (size < 50) {
+            System.out.println(name + " says: Ruff!");
+        } else {
+            System.out.println(name + " says: Bark!");
+        }
+    }
+}
