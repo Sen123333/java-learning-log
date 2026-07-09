ca7c8b05789b83ca9ec1bfab861d7f5d1251ace5
Sen123333 <sskcqw11@gmail.com>
2026-07-09 06:47:53 -0400

    Update PrimitivePractice.java

--- week01-java-basics/   day04-variables-references/src/PrimitivePractice.java
+++ week01-java-basics/   day04-variables-references/src/PrimitivePractice.java
@@ -1,1 +1,23 @@
+public class PrimitivePractice {
+    public static void main(String[] args) {
+        int age = 18;
+        double price = 9.99;
+        boolean isStudent = true;
+        char grade = 'A';
 
+        System.out.println("age = " + age);
+        System.out.println("int stores whole numbers.");
+        System.out.println();
+
+        System.out.println("price = " + price);
+        System.out.println("double stores decimal numbers.");
+        System.out.println();
+
+        System.out.println("isStudent = " + isStudent);
+        System.out.println("boolean stores true or false.");
+        System.out.println();
+
+        System.out.println("grade = " + grade);
+        System.out.println("char stores one character.");
+    }
+}
