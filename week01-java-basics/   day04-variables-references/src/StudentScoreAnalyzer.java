b4d6c28361492f9464a5ff1db4203eb34a447e30
Sen123333 <sskcqw11@gmail.com>
2026-07-09 06:48:31 -0400

    Update StudentScoreAnalyzer.java

--- week01-java-basics/   day04-variables-references/src/StudentScoreAnalyzer.java
+++ week01-java-basics/   day04-variables-references/src/StudentScoreAnalyzer.java
@@ -1,1 +1,38 @@
+public class StudentScoreAnalyzer {
+    public static void main(String[] args) {
+        int[] scores = {95, 70, 55, 80, 40};
 
+        int highest = scores[0];
+        int lowest = scores[0];
+        int total = 0;
+        int passingCount = 0;
+
+        System.out.print("Scores: ");
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(scores[i] + " ");
+
+            if (scores[i] > highest) {
+                highest = scores[i];
+            }
+
+            if (scores[i] < lowest) {
+                lowest = scores[i];
+            }
+
+            total = total + scores[i];
+
+            if (scores[i] >= 60) {
+                passingCount++;
+            }
+        }
+
+        double average = total / (double) scores.length;
+
+        System.out.println();
+        System.out.println("Highest score: " + highest);
+        System.out.println("Lowest score: " + lowest);
+        System.out.println("Average score: " + average);
+        System.out.println("Passing students: " + passingCount);
+    }
+}
