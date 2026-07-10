95b26f3e9b94192c0ad8463c99d599b35c90ea8a
Sen123333 <sskcqw11@gmail.com>
2026-07-10 08:02:00 -0400

    Update BankAccount.java

--- week01-java-basics/   day05-how-objects-behave/src/BankAccount.java
+++ week01-java-basics/   day05-how-objects-behave/src/BankAccount.java
@@ -1,1 +1,29 @@
+public class BankAccount {
+    private double balance;
 
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean deposit(double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
+        balance = balance + amount;
+        return true;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
+        if (amount > balance) {
+            return false;
+        }
+
+        balance = balance - amount;
+        return true;
+    }
+}
