4e1d0cf72ad2a47f36725c88e3570ec5fc170b44
Sen123333 <sskcqw11@gmail.com>
2026-07-10 08:01:45 -0400

    Update DogTestDrive.java

--- week01-java-basics/   day05-how-objects-behave/src/DogTestDrive.java
+++ week01-java-basics/   day05-how-objects-behave/src/DogTestDrive.java
@@ -1,1 +1,25 @@
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d = new Dog();
 
+        d.setName("Max");
+        d.setSize(10);
+
+        d.printInfo();
+        d.bark();
+
+        System.out.println();
+
+        System.out.println("Change size to 60.");
+        d.setSize(60);
+        d.printInfo();
+        d.bark();
+
+        System.out.println();
+
+        System.out.println("Try to set size to -5.");
+        d.setSize(-5);
+        d.printInfo();
+        d.bark();
+    }
+}
