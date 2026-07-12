3f0020d19e95768d7d81470f5b8aaebd1c7408ff
Sen123333 <sskcqw11@gmail.com>
2026-07-12 06:18:59 -0400

    Update RandomRangeDemo.java

--- week01-java-basics/day06-extra-strength-methods/src/RandomRangeDemo.java
+++ week01-java-basics/day06-extra-strength-methods/src/RandomRangeDemo.java
@@ -1,1 +1,22 @@
+public class RandomRangeDemo {
+    public static void main(String[] args) {
+        boolean allPass = true;
 
+        for (int i = 0; i < 20; i++) {
+            int randomStart = (int) (Math.random() * 5);
+
+            if (randomStart >= 0 && randomStart <= 4) {
+                System.out.println("Random start: " + randomStart + " PASS");
+            } else {
+                System.out.println("Random start: " + randomStart + " FAIL");
+                allPass = false;
+            }
+        }
+
+        if (allPass) {
+            System.out.println("Final result: PASS");
+        } else {
+            System.out.println("Final result: FAIL");
+        }
+    }
+}
