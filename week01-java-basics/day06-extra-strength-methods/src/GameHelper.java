efea39f46ff028176942bc9aaab2b452a68246bf
Sen123333 <sskcqw11@gmail.com>
2026-07-12 06:18:38 -0400

    Update GameHelper.java

--- week01-java-basics/day06-extra-strength-methods/src/GameHelper.java
+++ week01-java-basics/day06-extra-strength-methods/src/GameHelper.java
@@ -1,1 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+
+        System.out.print(prompt);
+
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+
+            inputLine = reader.readLine();
+
+            if (inputLine.length() == 0) {
+                return "invalid";
+            }
+        } catch (IOException ex) {
+            System.out.println("IOException: " + ex);
+            return "invalid";
+        }
+
+        return inputLine;
+    }
+}
