11fa970e814ce8b0d373d513920dcfaaa6b3d5c2
Sen123333 <sskcqw11@gmail.com>
2026-07-12 06:18:22 -0400

    Update SimpleDotComGame.java

--- week01-java-basics/day06-extra-strength-methods/src/SimpleDotComGame.java
+++ week01-java-basics/day06-extra-strength-methods/src/SimpleDotComGame.java
@@ -1,1 +1,29 @@
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
 
+        SimpleDotCom dot = new SimpleDotCom();
+
+        int randomStart = (int) (Math.random() * 5);
+        int[] locations = {randomStart, randomStart + 1, randomStart + 2};
+
+        dot.setLocationCells(locations);
+
+        boolean isAlive = true;
+
+        while (isAlive) {
+            String guess = helper.getUserInput("Enter a number: ");
+            String result = dot.checkYourself(guess);
+
+            numOfGuesses++;
+
+            System.out.println(result);
+
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + " guesses");
+            }
+        }
+    }
+}
