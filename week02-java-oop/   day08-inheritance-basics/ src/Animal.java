3e563de16e8a733692351f8c694ed064cde2ff41
Sen123333 <sskcqw11@gmail.com>
2026-07-18 08:47:56 -0400

    Update Animal.java

--- week02-java-oop/   day08-inheritance-basics/ src/Animal.java
+++ week02-java-oop/   day08-inheritance-basics/ src/Animal.java
@@ -1,1 +1,34 @@
+public class Animal {
+    private String name;
+    private int age;
 
+    public Animal(String name, int age) {
+        if (name == null || name.trim().length() == 0) {
+            this.name = "Unknown";
+        } else {
+            this.name = name.trim();
+        }
+
+        if (age < 0) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String move() {
+        return name + " moves";
+    }
+
+    public String makeSound() {
+        return "Some animal sound";
+    }
+}
