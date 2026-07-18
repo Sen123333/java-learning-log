332bd762a146caeaa7f3a799e33ffcac216e3701
Sen123333 <sskcqw11@gmail.com>
2026-07-18 08:48:24 -0400

    Update Bird.java

--- week02-java-oop/   day08-inheritance-basics/ src/Bird.java
+++ week02-java-oop/   day08-inheritance-basics/ src/Bird.java
@@ -1,1 +1,34 @@
+public class Bird extends Animal {
+    private boolean canFly;
 
+    public Bird(String name, int age, boolean canFly) {
+        super(name, age);
+        this.canFly = canFly;
+    }
+
+    public boolean canFly() {
+        return canFly;
+    }
+
+    @Override
+    public String makeSound() {
+        return "Tweet";
+    }
+
+    @Override
+    public String move() {
+        if (canFly) {
+            return getName() + " flies through the air";
+        } else {
+            return getName() + " walks on the ground";
+        }
+    }
+
+    public String fly() {
+        if (canFly) {
+            return getName() + " is flying";
+        } else {
+            return getName() + " cannot fly";
+        }
+    }
+}
