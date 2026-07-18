8bf4416fb734294aa91c6e5a79c6bdf58af7293b
Sen123333 <sskcqw11@gmail.com>
2026-07-18 08:48:15 -0400

    Update Cat.java

--- week02-java-oop/   day08-inheritance-basics/ src/Cat.java
+++ week02-java-oop/   day08-inheritance-basics/ src/Cat.java
@@ -1,1 +1,14 @@
+public class Cat extends Animal {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
 
+    @Override
+    public String makeSound() {
+        return "Meow";
+    }
+
+    public String scratch() {
+        return getName() + " scratches the sofa";
+    }
+}
